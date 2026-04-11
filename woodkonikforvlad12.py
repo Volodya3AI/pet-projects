8408880f903fdb0c7a0939b0b66d493d9479539c
Volodya3AI <vovastrasko5@gmail.com>
2026-04-11 18:37:59 +0300

    надіюсь зальється

---
+++ woodkonikforvlad12.py
@@ -0,0 +1,8 @@
+import webbrowser
+import time
+
+# Робимо невеличку паузу, щоб друг нічого не запідозрив
+time.sleep(10)
+
+# Відкриваємо посилання (можна вставити будь-яке, наприклад, з Ріком Естлі)
+webbrowser.open("https://www.youtube.com/watch?v=EIXctSrNxWA&list=RDEIXctSrNxWA&start_radio=1")
