ce403d329435463ad9af4bfd4eb12b754d4b878c
Volodymyr Strashko <vovastrasko5@gmail.com>
2026-05-05 09:52:56 +0300

    Delete woodkonikforvlad12.py

--- woodkonikforvlad12.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-import time
-
-# Робимо невеличку паузу, щоб друг нічого не запідозрив
-time.sleep(10)
-
-# Відкриваємо посилання (можна вставити будь-яке, наприклад, з Ріком Естлі)
-webbrowser.open("https://www.youtube.com/watch?v=EIXctSrNxWA&list=RDEIXctSrNxWA&start_radio=1")
